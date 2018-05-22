763b5268c7a1c74134d3a65f8a8796f9f6e4f214
Xmgty <36451940+Xmgty@users.noreply.github.com>
2018-05-22 17:53:39 +0300

    Add files via upload

--- Classesfor.py
+++ Classesfor.py
@@ -1,11 +1,33 @@
 import pygame
 import random
+import sys
 
 WIDTH = 900
 HEIGHT = 700
 FPS = 60
 pygame.init()
-screen = pygame.Surface((WIDTH, HEIGHT))
+window = pygame.display.set_mode((WIDTH, HEIGHT))
+screen = pygame.Surface((900, 700))
+info = pygame.Surface((900, 700))
+pygame.display.set_caption('Space Invaders')
+black = (0,0,0)
+white = (255, 255, 255)
+yellow = (255, 255, 30)
+red = (255, 0, 0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -77,9 +99,9 @@ class Enemy(pygame.sprite.Sprite):
         self.dy = 6
         self.centerx = random.randint(100, 800)
         self.centery = 0
-        self.dx = random.randrange(-2, 2)
-        self.height = 30
-        self.width = 150
+        self.dx = random.randrange(-4, 4)
+        self.height = 20
+        self.width = 50
         self.rect = pygame.Rect(self.centerx - self.width / 2, self.centery - self.height / 2, self.width, self.height)
 
 
@@ -97,6 +119,7 @@ class Enemy(pygame.sprite.Sprite):
         if pygame.sprite.groupcollide(enemySprites, playerSprite, 1, 1):
            explosionSprites.add(EnemyExplosion(self.rect.center))
            explosionSprites.add(PlayerExplosion(self.rect.center))
+
 
 
 
@@ -121,6 +144,7 @@ class Enemy(pygame.sprite.Sprite):
                 enemySprites.add(Enemy(400))
             if r == 3:
                 enemySprites.add(Enemy(200))
+                enemySprites.add(Enemy(500))
 
 
 class EnemyExplosion(pygame.sprite.Sprite):
@@ -154,16 +178,17 @@ class PlayerExplosion(pygame.sprite.Sprite):
         self.counter = self.counter + 1
         if self.counter == self.maxcount:
             self.kill()
-            exit()
+            pygame.mixer.music.stop()
+            game.menu()
 
 def main():
-# Initialize Everything
+# Фон загрузка
     background = pygame.image.load('images/I5ram.png')
     background_size = background.get_size()
     background_rect = background.get_rect()
     screen = pygame.display.set_mode(background_size)
 
-    pygame.display.set_caption('UoN Invaders')
+
     w, h = background_size
     x = 0
     y = 0
@@ -171,7 +196,6 @@ def main():
     x1 = 0
     y1 = -h
 
-    screen.blit(background, background_rect)
 
 
 # МУЗЫКА
@@ -187,7 +211,7 @@ def main():
     clock = pygame.time.Clock()
 
     i = 0
-    i += 1
+
 
 
 
@@ -231,6 +255,8 @@ def main():
     going = True
 
     while going:
+
+
 
         # ФОН,БГ
         y1 += 5
@@ -286,10 +312,13 @@ def main():
             if event.type == pygame.QUIT:
 
                 going = False
+                exit()
 
             elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
 
                 going = False
+                pygame.mixer.music.stop()
+                game.menu()
 
         # ОБНОВЛЕНИЯ
         player.update()
@@ -322,6 +351,68 @@ def main():
 
     pygame.quit()
 
-if __name__ == '__main__':
 
-    main()
+
+
+
+class Menu:
+    def __init__(self, punkts=[400, 350, u'Punkt', (250, 250, 30), (255, 255, 30)]):
+        self.punkts = punkts
+
+    def render(self, poverhnost, font, num_punkt):
+        for i in self.punkts:
+            if num_punkt == i[5]:
+                poverhnost.blit(font.render(i[2], 1, i[4]), (i[0], i[1] - 30))
+            else:
+                poverhnost.blit(font.render(i[2], 1, i[3]), (i[0], i[1] - 30))
+
+    def menu(self):
+        done = True
+        font_menu = pygame.font.Font(None, 90)
+        pygame.key.set_repeat(0, 0)
+        pygame.mouse.set_visible(True)
+        punkt = 0
+        while done:
+            info.fill((0, 0, 0))
+            screen.fill((0, 0, 0))
+
+            mp = pygame.mouse.get_pos()
+            for i in self.punkts:
+                if mp[0] > i[0] and mp[0] < i[0] + 155 and mp[1] > i[1] and mp[1] < i[1] + 50:
+                    punkt = i[5]
+            self.render(screen, font_menu, punkt)
+            for e in pygame.event.get():
+                if e.type == pygame.QUIT:
+                    sys.exit()
+                if e.type == pygame.KEYDOWN:
+                    if e.key == pygame.K_ESCAPE:
+                        sys.exit()
+                    if e.key == pygame.K_UP:
+                        if punkt > 0:
+                            punkt -= 1
+                    if e.key == pygame.K_DOWN:
+                        if punkt < len(self.punkts) - 1:
+                            punkt += 1
+                if e.type == pygame.MOUSEBUTTONDOWN and e.button == 1:
+                    if punkt == 0:
+                        done = False
+                        if __name__ == '__main__':
+                            main()
+                    elif punkt == 1:
+                        exit()
+            window.blit(info, (0, 0))
+            window.blit(screen, (0, 0))
+            pygame.display.flip()
+
+
+
+
+
+
+pygame.font.init()
+
+punkts = [(360, 300, u'PLAY', (255, 255, 255), (0, 0, 255), 0),
+          (360, 380, u'EXIT', (255, 255, 255), (255, 0, 0), 1),
+          (200, 100, u'Space Invaders', (250, 250, 30), (250, 250, 30), 2)]
+game = Menu(punkts)
+game.menu()
